5dc027a7ffdfc6a6c1dcc08c7187c4fd31bbf681
Crointsu <68754875+Crointsu@users.noreply.github.com>
2022-03-09 23:28:50 +0000

    changed token

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 for i in range(len(cogs)):
     cogs[i].setup(client)
 
-client.run("NjY2NDIxODA2NDI4ODQ4MTMx.Xhz7rA.FJtzolEsYmVXDEOF_kebYgFXW9E")
+client.run("NjY2NDIxODA2NDI4ODQ4MTMx.Xhz7rA.YMv3DOU3klBwMYzHU5K68eDukR8")
